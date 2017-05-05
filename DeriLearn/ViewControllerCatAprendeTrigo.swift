02763998524f1e3c5b27c68f54643d8cbd9e0e7a
MiguelBazan <miguelb1995@icloud.com>
2017-05-05 14:23:11 -0500

    Version final

--- DeriLearn/ViewControllerCatAprendeTrigo.swift
+++ DeriLearn/ViewControllerCatAprendeTrigo.swift
@@ -32,7 +32,7 @@ class ViewControllerCatAprendeTrigo: UIViewController {
             let listaDesc : [String] = ["Tenemos nuestra función original, y lo único que nos importa realmente es lo que esta dentro de los paréntesis.", "Se deriva el ángulo, que es lo que esta dentro del paréntesis.", "En este caso el resultado es 6. Esto lo multiplicaremos a la nueva función.", "Por regla, después de la derivación, se cambia Seno por Coseno."]
             let listaPasos2 : [String] = ["1er Paso", "2do Paso", "3er Paso", "4to Paso", "5to Paso"]
             let listaFunciones2 : [UIImage] = [#imageLiteral(resourceName: "sin21"), #imageLiteral(resourceName: "sin22"), #imageLiteral(resourceName: "sin23"), #imageLiteral(resourceName: "sin24"), #imageLiteral(resourceName: "sin25")]
-            let listaDesc2 : [String] = ["Desc Paso 1", "Desc Paso 2", "Desc Paso 3", "Desc Paso 4","Desc Paso 5"]
+            let listaDesc2 : [String] = ["Tenemos nuestra función original, y lo único que nos importa realmente es lo que esta dentro de los paréntesis.", "Se deriva el ángulo, que es lo que esta dentro del paréntesis.", "Tenemos este termino y se tiene que derivar para poder seguir", "Este es el resultado de la derivacion del termino anterior","Finalmente por regla Seno se cambia por Coseno."]
             
             let dest = segue.destination as! ViewController
             dest.listaPasos = listaPasos
@@ -48,7 +48,7 @@ class ViewControllerCatAprendeTrigo: UIViewController {
             let listaDesc : [String] = ["Tenemos nuestra función original, y lo único que nos importa realmente es lo que esta dentro de los paréntesis.", "Se deriva el ángulo, que es lo que esta dentro del paréntesis.", "En este caso el resultado es 4. Esto lo multiplicaremos a la nueva función.", "Por regla, después de la derivación, se cambia Coseno por -Seno."]
             let listaPasos2 : [String] = ["1er Paso", "2do Paso", "3er Paso", "4to Paso", "5to Paso"]
             let listaFunciones2 : [UIImage] = [#imageLiteral(resourceName: "cos21"), #imageLiteral(resourceName: "cos22"), #imageLiteral(resourceName: "cos23"), #imageLiteral(resourceName: "cos24"), #imageLiteral(resourceName: "cos25")]
-            let listaDesc2 : [String] = ["Desc Paso 1", "Desc Paso 2", "Desc Paso 3", "Desc Paso 4","Desc Paso 5"]
+            let listaDesc2 : [String] = ["Tenemos nuestra función original, y lo único que nos importa realmente es lo que esta dentro de los paréntesis.", "Se deriva el ángulo, que es lo que esta dentro del paréntesis.", "Tenemos este termino y se tiene que derivar para poder seguir", "Este es el resultado de la derivacion del termino anterior","Por regla se cambia Coseno por Seno y se hace negativo."]
             
             let dest = segue.destination as! ViewController
             dest.listaPasos = listaPasos
@@ -61,10 +61,10 @@ class ViewControllerCatAprendeTrigo: UIViewController {
         } else if segue.identifier == "tan" {
             let listaPasos : [String] = ["1er Paso", "2do Paso", "3er Paso", "4to Paso"]
             let listaFunciones : [UIImage] = [#imageLiteral(resourceName: "tan1"), #imageLiteral(resourceName: "tan2"), #imageLiteral(resourceName: "tan3"), #imageLiteral(resourceName: "tan4")]
-            let listaDesc : [String] = [" Tenemos nuestra función original, y lo único que nos importa realmente es lo que esta dentro de los paréntesis.", "Se deriva el ángulo, que es lo que esta dentro del paréntesis.", "En este caso el resultado es 2, esto lo multiplicaremos a la nueva función.", "Por regla, después de la derivación, se cambia Tan por Secante al cuadrado."]
+            let listaDesc : [String] = ["Tenemos nuestra función original, y lo único que nos importa realmente es lo que esta dentro de los paréntesis.", "Se deriva el ángulo, que es lo que esta dentro del paréntesis.", "En este caso el resultado es 2, esto lo multiplicaremos a la nueva función.", "Por regla, después de la derivación, se cambia Tan por Secante al cuadrado."]
             let listaPasos2 : [String] = ["1er Paso", "2do Paso", "3er Paso", "4to Paso", "5to Paso"]
             let listaFunciones2 : [UIImage] = [#imageLiteral(resourceName: "tan21"), #imageLiteral(resourceName: "tan22"), #imageLiteral(resourceName: "tan23"), #imageLiteral(resourceName: "tan24"), #imageLiteral(resourceName: "tan25")]
-            let listaDesc2 : [String] = ["Desc Paso 1", "Desc Paso 2", "Desc Paso 3", "Desc Paso 4","Desc Paso 5"]
+            let listaDesc2 : [String] = ["Tenemos nuestra función original, y lo único que nos importa realmente es lo que esta dentro de los paréntesis.", "Se deriva el ángulo, que es lo que esta dentro del paréntesis.", "Tenemos Seno como termino y se tiene que derivar para poder seguir", "Coseno es el resultado de la derivacion del termino anterior","Por regla se cambia Tangente por Cos(u)Sec^2(u)."]
             
             let dest = segue.destination as! ViewController
             dest.listaPasos = listaPasos
